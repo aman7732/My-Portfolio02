e0edd143e6e43870ffd9093d3f14d37a2887f969
aman7732 <132009495+aman7732@users.noreply.github.com>
2024-04-11 01:07:29 +0530

    minor issue resolve regarding to the form

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -32,7 +32,7 @@ export default function Form() {
     const toastId = toast.loading("Sending your message, please wait...");
 
     toast.info(
-      "Form submissions are demo-only here. Please checkout the final code repo to enable it. If you want to connect you can reach out to me via codebucks27@gmail.com.",
+      "Form submissions are demo-only here. Please checkout the final code repo to enable it. If you want to connect you can reach out to me via amanmishra12723@gmail.com",
       {
         id: toastId,
       }
